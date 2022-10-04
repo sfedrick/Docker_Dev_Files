e5da9ba9771029c19375e154b18a1e4382fd90f7
shaun <fedrick.shaun1234@gmail.com>
2022-10-03 22:21:51 -0400

    fix init script

--- Dev_Esp_32/init_script.sh
+++ Dev_Esp_32/init_script.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 # run when you first start the container with ./init_script.sh
-cd /home/esp-idf/
-./install.sh
-. ./export.sh
+cd /home/ 
+. /esp-idf/install.sh
+. ./esp-idf/export.sh
+cd -
