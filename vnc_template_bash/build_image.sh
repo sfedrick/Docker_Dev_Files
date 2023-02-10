bb5453489ddd06b2604df60db3d8161a5d9529a3
shaun <fedrick.shaun1234@gmail.com>
2023-02-10 08:55:41 -0500

    vnc lite is a better template

--- vnc_template_bash/build_image.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-docker build -t docker_vnc_template .
--- vnc_template_bash/run_image.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cd ..
-dir_to_mount=$(pwd)
-docker run --gpus device=0 -d -p 5901:5901 -p 6901:6901 -v $dir_to_mount:/shared_drive docker_desktop
